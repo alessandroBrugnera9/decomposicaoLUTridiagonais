54fa2ae0986312ddf028e98fa60c437f98a64c6b
alessandro.Brugnera <alessandrobrugnera.s@gmail.com>
2022-04-06 18:07:21 +0200

    basic functions ready

--- main.py
+++ main.py
@@ -2,110 +2,61 @@ from typing import Tuple
 import numpy as np
 
 
-def checkTridiagional(matrix: np.ndarray) -> bool:  # TODO: receive np matrix
-    size = len(matrix)
-    if(size != len(matrix[0])):
-        if (verbose):
-            print("Non square Matrix")
-        return False
-
-    for i in range(size):
-        for j in range(size):
-            if matrix[i][j] != 0:
-                if (i == j):
-                    continue
-                elif (i-1 == j):
-                    continue
-                elif (i == j-1):
-                    continue
-                else:
-                    return (False)
-    return True
-
-
-def checkUpperTriangualar(matrix: np.ndarray) -> bool:  # TODO: receive np matrix
-    size = len(matrix)
-    if(size != len(matrix[0])):
-        if (verbose):
-            print("Non square Matrix")
-        return False
-
-    for i in range(size):
-        for j in range(size):
-            if matrix[i][j] != 0:
-                if (i == j):
-                    continue
-                elif (i < j):
-                    continue
-                else:
-                    return False
-    return True
-
-
-def checkLowerTriangualar(matrix: np.ndarray) -> bool:  # TODO: receive np matrix
-    size = len(matrix)
-    if(size != len(matrix[0])):
-        if (verbose):
-            print("Non square Matrix")
-        return False
-
-    for i in range(size):
-        for j in range(size):
-            if matrix[i][j] != 0:
-                if (i == j):
-                    continue
-                elif (i > j):
-                    continue
-                else:
-                    return False
-    return True
-
-
-def checkLU(lMatrix, uMatrix) -> bool:  # TODO receive 2 matrixes
-    if (not checkLowerTriangualar(lMatrix)):
-        if (verbose):
-            print("L not lower")
-        return False
-
-    if (not checkUpperTriangualar(uMatrix)):
-        if (verbose):
-            print("U not upper")
-        return False
-
-    return True
-
-
-def buildTestSystem(size: int) -> Tuple[np.ndarray, np.ndarray]:
-    aMatrix = np.zeros((size, size))
-    bMatrix = np.zeros((size, 1))
+def buildTestSystem(size: int, cyclic: bool = False) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    aVector = np.zeros(size)
+    bVector = np.zeros(size)
+    cVector = np.zeros(size)
+    dVector = np.zeros(size)
 
     for n in range(size-1):
         upperValue = (2*(n+1)-1)/(4*(n+1))
-        # main diagonal
-        aMatrix[n][n] = 2
-
-        # upper diagonal
-        aMatrix[n][n+1] = 1 - upperValue
-
-        # lower diagonal
-        aMatrix[n][n-1] = upperValue
-
-        bMatrix[n][0] = np.cos(2*np.pi*(n**2)/(size**2))
+        aVector[n] = 1 - upperValue
+        bVector[n] = 2
+        cVector[n] = upperValue
+        dVector[n] = np.cos(2*np.pi*((n+1)**2)/(size**2))
     # overriding last entry
     upperValue = (2*size-1)/(4*size)
-    aMatrix[size-1][size-1] = 2
-    aMatrix[size-1][0] = 1 - upperValue
-    aMatrix[size-1][size-1-1] = upperValue
+    aVector[size-1] = 1 - upperValue
+    bVector[size-1] = 2
+    cVector[size-1] = upperValue
+    dVector[size-1] = 1
 
-    return aMatrix, bMatrix
+    if(not cyclic):
+        aVector[0] = 0
+        cVector[-1] = 0
+
+    return (aVector, bVector, cVector, dVector)
 
 
 verbose = True
 
 
+def LUDecomposition(aVector, bVector, cVector, L, U):
+    # the tridigonalmatrix is represented using only 3 vectors
+    n = len(aVector)
+    U[0] = bVector[0]
+    for i in range(1, n, 1):
+        L[i] = aVector[i]/U[i-1]
+        U[i] = bVector[i]-L[i]*cVector[i-1]
+    return
+
+
+def resolve_LU_tridiagonal(L, U, d, c, x, y):
+    # L,U,d,c,x,y são vetores // este algoritmo supõe uma matriz nxn //
+    n = len(L)
+    y[0] = d[0]
+    for i in range(1, n, 1):
+        y[i] = d[i]-L[i]*y[i-1]
+    x[n-1] = y[n-1]/U[n-1]
+    for j in range(n-2, -1, -1):
+        x[j] = (y[j]-c[j]*x[j+1])/U[j]
+    return
+
+
 def main():
     # TODO: define condition for verbose
-    m=buildTestSystem(6)[0]
+    aVector, bVector, cVector, dVector = buildTestSystem(6)
+    print(aVector, bVector, cVector, dVector)
 
-    
+
 main()
